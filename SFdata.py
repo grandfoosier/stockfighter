8cdc7352168e76881a6b0a62e69d8c22b79e0f9d
grandfoosier <rice.steve@gmail.com>
2016-01-29 16:24:56 -0500

    Create SFdata.py

---
+++ SFdata.py
@@ -0,0 +1,9 @@
+apikey = "my key"
+auth = {"X-Starfighter-Authorization": apikey}
+base_url = "https://api.stockfighter.io/ob/api"
+base_urlws = "wss://api.stockfighter.io/ob/api"
+
+account = "ACCT12345"
+stock = "STOCK"
+venue = "THATEX"
+instance = 99999
