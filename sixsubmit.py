6b68c2b0c2ed8b4bb2ca9280b0641eadc3c463e3
grandfoosier <rice.steve@gmail.com>
2016-01-29 11:04:41 -0500

    Create sixsubmit.py

---
+++ sixsubmit.py
@@ -0,0 +1,26 @@
+import web
+import requests
+import json
+from ws4py.client.threadedclient import WebSocketClient
+import SFdata
+import sys
+
+apikey = SFdata.apikey
+auth = SFdata.auth
+base_url = SFdata.base_url
+base_urlws = SFdata.base_urlws
+
+account = SFdata.account
+stock = SFdata.stock
+venue = SFdata.venue
+
+perp = "RAH5051135"
+
+link = ""
+
+ evidence = { "account" : sys.argv1, "explanation_link": link, 
+              "executive_summary": "" }
+
+/gm/instances/YOUR_INSTANCE_ID/judge
+
+/gm/instances/YOUR_INSTANCE_ID
