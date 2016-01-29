dc8d5eae76f1c254eb91ff0d835c94ab0b6ea5ce
grandfoosier <rice.steve@gmail.com>
2016-01-29 12:05:13 -0500

    Update sixsubmit.py
    had perp in link instead of instance. oops!

--- sixsubmit.py
+++ sixsubmit.py
@@ -14,28 +14,31 @@ instance = SFdata.instance
 
 perp = "RAH5051135"
 
-url = base_url + perp + "/judge"
+url = base_url + str(instance) + "/judge"
 
 link = "https://docs.google.com/document/d/1cWRproRq_FwsX6Lmm-s_Xvb9IeNLX_DeJ-vcTX-QRMk"
 
 summary = """
 
- To summarize, my three pieces of evidence pointing to account 
- RAH5051135 as the anomaly were:
+To summarize, my three pieces of evidence pointing to account 
+RAH5051135 as the anomaly were:
 
- 1. It had the largest (fractional) discrepancy between average bid quantity 
-      and average ask quantity - 251 to 1,425, respectively. 
-      This is what led me to piece #2.
- 2. It was the only account to repeatedly return to the same position after 
-      selling off a large number of shares. Although I can’t be certain, 
-      I would bet that it was selling to a position of zero.
- 3. It had the highest gained value per trade, and per share traded, 
-      among all accounts on the exchange.
+1. It had the largest (fractional) discrepancy between average bid quantity 
+     and average ask quantity - 251 to 1,425, respectively. 
+     This is what led me to piece #2. 
+2. It was the only account to repeatedly return to the same position after
+     selling off a large number of shares. Although I can't be certain, 
+     I would bet that it was selling to a position of zero. 
+3. It had the highest gained value per trade, and per share traded, 
+     among all accounts on the exchange. 
 
- """
+"""
 
-evidence = { "account" : perp, "explanation_link": link, 
-              "executive_summary": summary }
+evidence = {
+  "account":perp,
+  "explanation_link":link, 
+  "executive_summary":summary
+}
 
 def post():
     p = requests.post(url, data=json.dumps(evidence), headers=auth)
@@ -44,3 +47,4 @@ def post():
 
 if __name__ == '__main__':
     presp = post()
+    print presp
