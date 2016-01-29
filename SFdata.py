02a8d9e2e3ca6a494554bc2d9de1b43937cf8c6b
grandfoosier <rice.steve@gmail.com>
2016-01-29 11:01:53 -0500

    Create SFdata.py

---
+++ SFdata.py
@@ -0,0 +1,8 @@
+apikey = "70619396e34793da29ef1ae554dde1a76c64a63e"
+auth = {"X-Starfighter-Authorization": apikey}
+base_url = "https://api.stockfighter.io/ob/api"
+base_urlws = "wss://api.stockfighter.io/ob/api"
+
+account = "CES94124070"
+stock = "FMCM"
+venue = "LYXCEX"
