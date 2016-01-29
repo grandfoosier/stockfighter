5ca5cced1f468241ac2d3001c690fa312f30e79d
grandfoosier <rice.steve@gmail.com>
2016-01-29 16:15:08 -0500

    Update six_data.py

--- six_data.py
+++ six_data.py
@@ -1,4 +1,4 @@
-apikey = "70619396e34793da29ef1ae554dde1a76c64a63e"
+apikey = "#######"
 auth = {"X-Starfighter-Authorization": apikey}
 base_url = "https://api.stockfighter.io/ob/api"
 base_urlws = "wss://api.stockfighter.io/ob/api"
