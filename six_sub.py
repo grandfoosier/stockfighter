78e53ea09a657acebf0ae4c6b2e984b2aae3b541
grandfoosier <rice.steve@gmail.com>
2016-01-29 14:41:56 -0500

    Update and rename wssub.py to six_sub.py
    name change for clarity

--- six_sub.py
+++ six_sub.py
@@ -3,17 +3,17 @@ import web
 import requests
 import json
 from ws4py.client.threadedclient import WebSocketClient
-import SFdata
+import six_data
 import sys
 
-apikey = SFdata.apikey
-auth = SFdata.auth
-base_url = SFdata.base_url
-base_urlws = SFdata.base_urlws
+apikey = six_data.apikey
+auth = six_data.auth
+base_url = six_data.base_url
+base_urlws = six_data.base_urlws
 
 account = sys.argv[1]
-stock = SFdata.stock
-venue = SFdata.venue
+stock = six_data.stock
+venue = six_data.venue
 
 path = "C:/users/20596566/mystuff/projects/stockfighter/docs/" + account + ".txt"
 acct_file = open(path, 'w')
