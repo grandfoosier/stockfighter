f50070c797b99a7e8e3bd1e35e6df47014147aed
grandfoosier <rice.steve@gmail.com>
2016-01-29 16:18:54 -0500

    Delete six_data.py

--- six_data.py
+++
@@ -1,9 +0,0 @@
-apikey = "#######"
-auth = {"X-Starfighter-Authorization": apikey}
-base_url = "https://api.stockfighter.io/ob/api"
-base_urlws = "wss://api.stockfighter.io/ob/api"
-
-account = "CES94124070"
-stock = "FMCM"
-venue = "LYXCEX"
-instance = 21986
